31886ae9882dbd47092d5a4d13bc54c813f0eec9
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-11-26 14:41:23 -0500

    ips al comienzo

--- nfs_install.sh
+++ nfs_install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 if [[ $(id -u) -eq 0 ]] ; then echo "This script must  not be excecuted as root or using sudo(althougth the user must be sudoer and password will be asked in some steps)" ; exit 1 ; fi
 
+echo "¿Cuál es la ip del servidor del web?"
+read ipweb
+
 sudo apt-get update
 sudo apt install nfs-kernel-server
 sudo mkdir /dc_storage /source_storage /web_storage
 sudo chown cubo:root /dc_storage /source_storage /web_storage
 
-echo "¿Cuál es la ip publica del servidor del web?"
-read ipweb
+
 sudo chmod o+w /etc/exports
 cat <<EOF >>/etc/exports
 /dc_storage     		192.168.0.0/16(rw,sync,no_subtree_check) $ipweb/32(rw,sync,no_subtree_check)
