bb339b568a974c3846f00241f8e6427162369ad8
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-09-25 15:39:06 -0500

    /run/airflow

--- airflow_install.sh
+++ airflow_install.sh
@@ -55,6 +55,8 @@ sed -i "s%celery_result_backend =.*%celery_result_backend = redis://$ipdb:6379/0
 sed -i "s%endpoint_url = .*%endpoint_url = http://$IP:8080%" "$AIRFLOW_HOME/airflow.cfg"
 sed -i "s%base_url = .*%base_url = http://$IP:8080%" "$AIRFLOW_HOME/airflow.cfg"
 
+sudo mkdir /run/airflow
+
 #MOUNT NFS SERVER
 cd $HOME
 
