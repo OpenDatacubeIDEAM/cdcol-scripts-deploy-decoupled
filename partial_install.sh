42f0ea7d7c7c2662337d886591fadd9662dfc01f
MariaMancipe <paula.mancipe@bcomcolombia.com>
2019-01-21 15:17:47 -0500

    (nuevo) rama open data cube y un script de instalación parcial

---
+++ partial_install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+if [[ $(id -u) -eq 0 ]] ; then echo "This script must  not be excecuted as root or using sudo(althougth the user must be sudoer and password will be asked in some steps)" ; exit 1 ; fi
+
+
+#IPS
+echo "¿Cuál es la ip del servidor de Bases de Datos?"
+read ipdb
+
+echo "¿Cuál es la ip del API REST?"
+read ipapi
+
+echo "¿Cuál es la ip del servidor NFS?"
+read ipnfs
+
+echo "¿Cuál es la ip pública de este servidor?"
+read IP
+
+#VARIABLES
+PASSWORD_AIRFLOW='cubocubo'
+USUARIO_CUBO="$(whoami)"
+PASSWORD_CUBO='ASDFADFASSDFA'
+ANACONDA_URL="https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh"
+OPEN_DATA_CUBE_REPOSITORY="https://github.com/opendatacube/datacube-core.git"
+BRANCH="desacoplado"
+
+
+
+while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
+   echo "Waiting while other process ends installs (dpkg/lock is locked)"
+   sleep 1
+done
+
+sudo apt install -y openssh-server postgresql-9.5 postgresql-client-9.5 postgresql-contrib-9.5 libgdal1-dev libhdf5-serial-dev libnetcdf-dev hdf5-tools netcdf-bin gdal-bin pgadmin3 libhdf5-doc netcdf-doc libgdal-doc git wget htop imagemagick ffmpeg|| exit 1
+
+#CONDA INSTALL
+if ! hash "conda" > /dev/null; then
+	mkdir -p ~/instaladores && wget -c -P ~/instaladores $ANACONDA_URL
+	bash ~/instaladores/Anaconda3-5.3.0-Linux-x86_64.sh -b -p $HOME/anaconda2
+	export PATH="$HOME/anaconda2/bin:$PATH"
+	echo 'export PATH="$HOME/anaconda2/bin:$PATH"'>>$HOME/.bashrc
+fi
+
+conda config --add channels conda-forge
+conda install -y psycopg2 gdal libgdal hdf5 rasterio netcdf4 libnetcdf pandas shapely ipywidgets scipy numpy
+
+git clone $OPEN_DATA_CUBE_REPOSITORY
+cd datacube-core
+python setup.py install
+
+cat <<EOF >~/.datacube.conf
+[datacube]
+db_database: datacube
+
+# A blank host will use a local socket. Specify a hostname to use TCP.
+db_hostname: $ipdb
+
+# Credentials are optional: you might have other Postgres authentication configured.
+# The default username otherwise is the current user id.
+db_username: $USUARIO_CUBO
+db_password: $PASSWORD_CUBO
+EOF
+
+datacube -v system init
+source $HOME/.bashrc
+
+cd $HOME
+
