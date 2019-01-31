4a886cfdbeeabae2a16613911bbd1c03eb7492b9
MariaMancipe <paula.mancipe@bcomcolombia.com>
2019-01-31 08:39:36 -0500

    incluyendo la instalación de airflow

--- partial_install.sh
+++ partial_install.sh
@@ -7,14 +7,14 @@ if [[ $(id -u) -eq 0 ]] ; then echo "This script must  not be excecuted as root 
 echo "¿Cuál es la ip del servidor de Bases de Datos?"
 read ipdb
 
-#echo "¿Cuál es la ip del API REST?"
-#read ipapi
+echo "¿Cuál es la ip del API REST?"
+read ipapi
 
 echo "¿Cuál es la ip del servidor NFS?"
 read ipnfs
 
-#echo "¿Cuál es la ip pública de este servidor?"
-#read IP
+echo "¿Cuál es la ip pública de este servidor?"
+read IP
 
 #VARIABLES
 PASSWORD_AIRFLOW='cubocubo'
@@ -91,6 +91,18 @@ if [[ -z "${AIRFLOW_HOME}" ]]; then
     echo "export AIRFLOW_HOME='$HOME/airflow'" >>"$HOME/.bashrc"
 fi
 
+
+airflow initdb
+sed -i "s%sql_alchemy_conn.*%sql_alchemy_conn = postgresql+psycopg2://airflow:$PASSWORD_AIRFLOW@$ipdb:5432/airflow%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%executor =.*%executor = CeleryExecutor%" "$AIRFLOW_HOME/airflow.cfg"
+
+sed -i "s%broker_url =.*%broker_url = amqp://airflow:airflow@$ipapi/airflow%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%celery_result_backend =.*%celery_result_backend = redis://$ipdb:6379/0%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%endpoint_url = .*%endpoint_url = http://$IP:8080%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%base_url = .*%base_url = http://$IP:8080%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%flower_port = .*%flower_port = 8082%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%load_examples = .*%load_examples = False%" "$AIRFLOW_HOME/airflow.cfg"
+
 #MOUNT NFS SERVER
 cd $HOME
 
@@ -109,4 +121,118 @@ sudo chown cubo:root /dc_storage /web_storage /source_storage
 sudo mount /dc_storage
 sudo mount /source_storage
 sudo mount /web_storage
+
+mkdir  /web_storage/{dags,plugins}
+
+ln -s /web_storage/dags "$AIRFLOW_HOME/dags"
+ln -s /web_storage/plugins "$AIRFLOW_HOME/plugins"
+touch /home/cubo/airflow/dags/dummy.py
+cat <<EOF >>/home/cubo/airflow/dags/dummy.py
+import airflow
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import timedelta
+args = {
+    'owner': 'airflow',
+    'start_date': airflow.utils.dates.days_ago(2)
+}
+dag = DAG(
+    dag_id='example_dummy', default_args=args,
+    schedule_interval=None,
+    dagrun_timeout=timedelta(minutes=1))
+run_this_last = DummyOperator(task_id='DOES_NOTHING', dag=dag)
+EOF
+
+
+airflow initdb
+
+#AIRFLOW SERVICE
+
+
+cd $HOME
+source .bashrc
+mkdir env
+cat <<EOF >>/home/cubo/env/airflow
+PATH="/home/cubo/anaconda2/bin:$PATH"
+AIRFLOW_HOME='/home/cubo/airflow'
+EOF
+
+sudo touch /etc/systemd/system/airflow-webserver.service
+sudo chmod o+w /etc/systemd/system/airflow-webserver.service
+cat <<EOF >/etc/systemd/system/airflow-webserver.service
+[Unit]
+Description=Airflow webserver daemon
+After=network.target
+
+
+[Service]
+EnvironmentFile=/home/cubo/env/airflow
+User=cubo
+Group=cubo
+Type=simple
+ExecStart= /home/cubo/anaconda2/bin/python /home/cubo/anaconda2/bin/airflow webserver
+Restart=on-failure
+RestartSec=5s
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo chmod o-w /etc/systemd/system/airflow-webserver.service
+
+
+sudo touch /etc/systemd/system/airflow-scheduler.service
+sudo chmod o+w /etc/systemd/system/airflow-scheduler.service
+cat <<EOF >/etc/systemd/system/airflow-scheduler.service
+[Unit]
+Description=Airflow scheduler daemon
+After=network.target
+
+
+[Service]
+EnvironmentFile=/home/cubo/env/airflow
+User=cubo
+Group=cubo
+Type=simple
+ExecStart=/home/cubo/anaconda2/bin/python /home/cubo/anaconda2/bin/airflow scheduler
+Restart=always
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo chmod o-w /etc/systemd/system/airflow-scheduler.service
+
+sudo touch /etc/systemd/system/flower.service
+sudo chmod o+w /etc/systemd/system/flower.service
+cat <<EOF >/etc/systemd/system/flower.service
+[Unit]
+Description=Airflow flower daemon
+After=network.target
+
+
+[Service]
+EnvironmentFile=/home/cubo/env/airflow
+User=cubo
+Group=cubo
+Type=simple
+ExecStart=/home/cubo/anaconda2/bin/python /home/cubo/anaconda2/bin/airflow flower
+Restart=on-failure
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo chmod o-w /etc/systemd/system/flower.service
+
+
+sudo systemctl daemon-reload
+sudo systemctl start airflow-webserver
+sudo systemctl enable airflow-webserver
+
+sudo systemctl start airflow-scheduler
+sudo systemctl enable airflow-scheduler
+
+sudo systemctl start flower
+sudo systemctl enable flower
 
