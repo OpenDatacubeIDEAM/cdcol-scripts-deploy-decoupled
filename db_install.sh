568b3daf33c5df07552d7f4edcc4171601ffeedc
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2019-03-16 18:41:04 -0500

    review db_install.sh

--- db_install.sh
+++ db_install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-if [[ $(id -u) -eq 0 ]] ; then echo "This script must  not be excecuted as root or using sudo(althougth the user must be sudoer and password will be asked in some steps)" ; exit 1 ; fi
-
-sudo apt-get update
+if [[ $(id -u) -eq 0 ]] 
+then 
+cat <<EOF
+This script must not be excecuted as root \
+or using sudo (althougth the user must be \
+sudoer and password will be asked in some steps)
+EOF
+exit 1
+fi
 
 IP=`hostname -I | awk '{ print $1 }'`
 echo IP
@@ -10,7 +16,22 @@ USUARIO_CUBO="$(whoami)"
 PASSWORD_CUBO='ASDFADFASSDFA'
 PASSWORD_AIRFLOW='cubocubo'
 
-sudo apt install -y openssh-server postgresql-9.5 postgresql-client-9.5 postgresql-contrib-9.5 libhdf5-serial-dev hdf5-tools pgadmin3 postgresql-doc-9.5 libhdf5-doc git wget htop rabbitmq-server || exit 1
+sudo apt-get update
+sudo apt install -y \
+	openssh-server \
+	postgresql-9.5 \
+	postgresql-client-9.5 \
+	postgresql-contrib-9.5 \
+	libhdf5-serial-dev \
+	hdf5-tools \
+	pgadmin3 \
+	postgresql-doc-9.5 \
+	libhdf5-doc \
+	git \
+	wget \
+	htop \
+	rabbitmq-server \
+	|| exit 1
 
 sudo -u postgres psql postgres<<EOF
 create user $USUARIO_CUBO with password '$PASSWORD_CUBO';
