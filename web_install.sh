cd641c96a072841f13b14b0dbe8010512f897c3c
Aurelio Vivas <aa.vivas@uniandes.edu.co>
2019-04-23 23:49:14 -0500

    adding python3.6.8 support

--- web_install.sh
+++ web_install.sh
@@ -10,83 +10,63 @@ read ipapi
 echo "¿Cuál es la ip del servidor NFS?"
 read ipnfs
 
-sudo apt-get update
+# Add hosts name to etc/hosts
+sudo sed "2i$ipdb    db" /etc/hosts
+sudo sed "2i$ipapi   api" /etc/hosts
+sudo sed "2i$ipnfs   nfs" /etc/hosts
 
-git clone -b desacoplado git@gitlab.virtual.uniandes.edu.co:datacube-ideam/CDCol.git
-mv CDCol/* ~/
+sudo apt update
+sudo apt install \
+  git \
+  postgresql \
+  postgresql-contrib \
+  nginx \
+  wget \
+  xz-utils \
+  build-essential \
+  zlib1g-dev \
+  libssl-dev
 
-USUARIO_CUBO="$(whoami)"
-PASSWORD_CUBO='ASDFADFASSDFA'
-ANACONDA_URL="https://repo.continuum.io/archive/Anaconda2-4.1.1-Linux-x86_64.sh"
-REPO="git@gitlab.virtual.uniandes.edu.co:datacube-ideam/agdc-v2.git"
-BRANCH="desacoplado"
+# Install CDCol
+git clone git@gitlab.virtual.uniandes.edu.co:datacube-ideam/CDCol.git -b desacoplado ~/CDCol
 
-if ! hash "conda" > /dev/null; then
-	mkdir -p ~/instaladores && wget -c -P ~/instaladores $ANACONDA_URL
-	bash ~/instaladores/Anaconda2-4.1.1-Linux-x86_64.sh -b -p $HOME/anaconda2
-	export PATH="$HOME/anaconda2/bin:$PATH"
-	echo 'export PATH="$HOME/anaconda2/bin:$PATH"'>>$HOME/.bashrc
-fi
-#Configurar Flower
+# Compile and Install Python 3.6.8
+# This installs 
+# * pip3.6
+# * python3.6
+wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tar.xz
+tar xf Python-3.6.8.tar.xz
+cd Python-3.6.8
+./configure
+make
+make test
+sudo make install
 
-conda install -c conda-forge flower celery=3.1.23
-sudo touch /etc/systemd/system/flower.service
-sudo chmod o+w /etc/systemd/system/flower.service
-cat <<EOF >/etc/systemd/system/flower.service
-[Unit]
-Description=flower daemon
-
-[Service]
-User=cubo
-Group=cubo
-WorkingDirectory=/home/cubo/cdcol_celery
-ExecStart=/home/cubo/anaconda2/bin/flower --broker=amqp://cdcol:cdcol@$ipapi/cdcol --port=8082 --loglevel=info
-Restart=on-failure
-Type=simple
-
-[Install]
-WantedBy=multi-user.target
-EOF
-sudo chmod o-w /etc/systemd/system/flower.service
-echo "Iniciando flower en el puerto 8082"
-sudo systemctl daemon-reload
-sudo systemctl start flower
-sudo systemctl enable flower
-
-sudo apt-get update
-sudo apt-get install python-pip python-dev libpq-dev postgresql postgresql-contrib nginx virtualenv gunicorn git
-
-# To avoid this error
-# OSError: [Errno 13] Permiso denegado: 
-# '/home/cubo/.cache/pip/wheels/ab/4f/e6/....
-sudo chown -R cubo:cubo /home/cubo/.cache
-sudo chown -R cubo:cubo /home/cubo/.conda
-
-pip install virtualenv
+# Install web application
+cd ~
+pip3.6 install virtualenv
 virtualenv v_ideam
-source v_ideam/bin/activate
-mkdir projects
-cd projects
-git clone git@gitlab.virtual.uniandes.edu.co:datacube-ideam/web-app.git
-cd web-app
-pip install -r requirements.txt
+source ~/v_ideam/bin/activate
 
-export IDEAM_DATABASE_URL="postgres://portal_web:CDCol_web_2016@$ipdb/ideam"
-export IDEAM_PRODUCTION_DATABASE_URL="postgres://portal_web:CDCol_web_2016@$ipdb/ideam"
-export IDEAM_API_URL="http://$ipapi:8000"
-export IDEAM_MAIL_HOST="smtp.gmail.com"
-export IDEAM_MAIL_USER="cdcolprueba@gmail.com"
-export IDEAM_MAIL_PASSWORD="ideam20162"
-export IDEAM_MAIL_PORT="587"
-export IDEAM_DC_STORAGE_PATH="/dc_storage"
-export IDEAM_WEB_STORAGE_PATH="/web_storage"
+git clone git@gitlab.virtual.uniandes.edu.co:datacube-ideam/web-app.git -b newDevelop ~/projects/web-app
+cd ~/projects/web-app
 
-echo IDEAM_DATABASE_URL
-echo $IDEAM_DATABASE_URL
+pip3.6 install -r requirements.txt
 
-python manage.py migrate
-python manage.py collectstatic
-python manage.py createsuperuser
+# Load web app env variables
+export $(egrep -v '^#' environment | xargs)
+
+python3.6 manage.py makemigrations
+python3.6 manage.py migrate
+python3.6 manage.py collectstatic
+
+# Loading application initial data
+python3.6 manage.py loaddata data/1.group.json
+python3.6 manage.py loaddata data/2.user.json
+python3.6 manage.py loaddata data/3.profile.json
+python3.6 manage.py loaddata data/4.topic.json
+
+deactivate
 
 sudo touch /etc/systemd/system/gunicorn.service
 sudo chmod o+w /etc/systemd/system/gunicorn.service
@@ -99,40 +79,23 @@ After=network.target
 User=cubo
 Group=cubo
 WorkingDirectory=/home/cubo/projects/web-app
-EnvironmentFile=/home/cubo/projects/web-app/.ideam.env
-ExecStart=/home/cubo/v_ideam/bin/gunicorn --timeout 36000 --bind 0.0.0.0:8080 ideam_cdc.wsgi:application
+EnvironmentFile=/home/cubo/projects/web-app/environment
+ExecStart=/home/cubo/v_ideam/bin/gunicorn --timeout 36000 --bind 0.0.0.0:8080 ideam.wsgi:application
  
 [Install]
 WantedBy=multi-user.target
 EOF
+
 sudo chmod o-w /etc/systemd/system/gunicorn.service
 
-cat <<EOF >>.ideam.env
-IDEAM_PRODUCTION_DATABASE_URL="postgres://portal_web:CDCol_web_2016@$ipdb/ideam"
-IDEAM_DATABASE_URL="postgres://portal_web:CDCol_web_2016@$ipdb/ideam"
-IDEAM_API_URL="http://$ipapi:8000"
-IDEAM_MAIL_HOST="smtp.gmail.com"
-IDEAM_MAIL_USER="cdcolprueba@gmail.com"
-IDEAM_MAIL_PASSWORD="ideam20162"
-IDEAM_MAIL_PORT="587"
-IDEAM_DC_STORAGE_PATH="/dc_storage"
-IDEAM_WEB_STORAGE_PATH="/web_storage"
-IDEAM_TEMPORIZER="3000"
-IDEAM_DAYS_ELAPSED_TO_DELETE_EXECUTION_RESULTS="360"
-IDEAM_ID_ALGORITHM_FOR_CUSTOM_SERVICE="8"
-EOF
-
-sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
 sudo systemctl daemon-reload
-sudo systemctl stop gunicorn
-sudo systemctl daemon-reload
 sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
 
-#Configuracion de Nginx
+# Configuring Nginx
 sudo touch /etc/nginx/sites-available/ideam
 sudo chmod o+w /etc/nginx/sites-available/ideam
+
 cat <<EOF >>/etc/nginx/sites-available/ideam
 server {
   listen 80;
@@ -170,25 +133,23 @@ server {
   }
 }
 EOF
+
 sudo chmod o-w /etc/nginx/sites-available/ideam
 
 sudo ln -s /etc/nginx/sites-available/ideam /etc/nginx/sites-enabled/
 sudo rm /etc/nginx/sites-enabled/default
 sudo nginx -t
 sudo systemctl restart nginx
-deactivate
 
 
-
-#MOUNT NFS SERVER
-
+# Mount NFS Server
 cd $HOME
 sudo apt install nfs-common
 sudo chmod o+w /etc/fstab
 cat <<EOF >>/etc/fstab
-$ipnfs:/source_storage	/source_storage nfs 	defaults    	0   	0
-$ipnfs:/dc_storage		/dc_storage 	nfs 	defaults    	0   	0
-$ipnfs:/web_storage   	/web_storage	nfs 	defaults    	0   	0
+nfs:/source_storage	/source_storage nfs 	defaults    	0   	0
+nfs:/dc_storage		/dc_storage 	nfs 	defaults    	0   	0
+nfs:/web_storage   	/web_storage	nfs 	defaults    	0   	0
 EOF
 sudo chmod o-w /etc/fstab
 
@@ -198,9 +159,3 @@ sudo mount /dc_storage
 sudo mount /source_storage
 sudo mount /web_storage
 cd $HOME
-
-#Configuracion CRON envio de correos
-cd projects/web-app/
-(crontab -l 2>/dev/null; echo "*/5 * * * * /home/cubo/projects/web-app/run.sh >> /home/cubo/projects/email_notifier.log 2>> /home/cubo/projects/email_notifier_error.log") | crontab -
-
-
