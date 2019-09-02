880059032e1faed09c5abdff4babfd424958574a
Aurelio Vivas <aa.vivas@uniandes.edu.co>
2019-09-01 21:28:30 -0500

    Allow remote conection automatically

--- 2.db_install.sh
+++ 2.db_install.sh
@@ -16,8 +16,8 @@ echo "=> La direccion IP actual es: $IP"
 
 echo "=> Instalacion de Paquetes ..."
 
-sudo apt-get update -qq -y
-sudo apt install -qq -y \
+sudo apt-get update -y
+sudo apt install -y \
 	openssh-server \
 	postgresql-9.5 \
 	postgresql-client-9.5 \
@@ -69,3 +69,25 @@ sudo -u postgres psql airflow -c "alter user airflow with encrypted password '$P
 sudo -u postgres psql airflow -c "grant all privileges on database airflow to airflow;"
 sudo systemctl restart postgresql.service
 
+# Getting Host IP address and CIDR mask. Ex: 192.168.205.4/24
+IP=$(ip -o -f inet addr show | awk '/scope global/ {print $4}')
+
+# Split address and CIDR mask
+IFS='/' read -r Address MaskCIDR <<< "$IP"
+
+# Convert CIDR mask in netmask. Ex: 24 -> 255.255.255.0
+# Reference: https://gist.github.com/kwilczynski/5d37e1cced7e76c7c9ccfdf875ba6c5b
+value=$(( 0xffffffff ^ ((1 << (32 - $MaskCIDR)) - 1) ))
+Mask="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
+
+# Getting the network given the Address and the mask. Ex: 192.168.123.1 , 255.255.255.0
+IFS=. read -r i1 i2 i3 i4 <<< "$Address"
+IFS=. read -r m1 m2 m3 m4 <<< "$Mask"
+Network="$((i1 & m1)).$((i2 & m2)).$((i3 & m3)).$((i4 & m4))"
+
+echo "=> Set listen_addresses = 'localhost' to '*' in '/etc/postgresql/9.5/main/postgresql.conf' "
+sed -i "s/^#listen_addresses = .*/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
+echo "=> Add line 'host    all             all             $Network\/$MaskCIDR          md5' to '/etc/postgresql/9.5/main/pg_hba.conf' "
+sudo sed -i '/host    all             all             ::1\/128                 md5/ a host    all             all             $Network\/$MaskCIDR          md5' /etc/postgresql/9.5/main/pg_hba.conf
+
+sudo systemctl restart postgresql.service
