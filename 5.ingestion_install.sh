0d22d4648a7e203e09c441be6e00dbd274d81bde
Aurelio Vivas <aa.vivas@uniandes.edu.co>
2019-09-03 09:51:54 -0500

    update

--- 5.ingestion_install.sh
+++ 5.ingestion_install.sh
@@ -4,11 +4,11 @@ if [[ $(id -u) -eq 0 ]] ; then echo "This script must  not be excecuted as root 
 echo "¿Cuál es la ip del servidor de Bases de Datos?"
 read ipdb
 
-echo "¿Cuál es la ip del servidor web?"
-read ipweb
-
 echo "¿Cuál es la ip del servidor NFS?"
 read ipnfs
+
+sudo sed -i "\$a$ipnfs   nfs" /etc/hosts
+sudo sed -i "\$a$ipdb    db" /etc/hosts
 
 sudo apt-get update
 
@@ -16,17 +16,35 @@ USUARIO_CUBO="$(whoami)"
 PASSWORD_CUBO='ASDFADFASSDFA'
 ANACONDA_URL="https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh"
 OPEN_DATA_CUBE_REPOSITORY="https://github.com/opendatacube/datacube-core.git"
-BRANCH="develop"
+BRANCH="datacube-1.6.1"
 
 while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
    echo "Waiting while other process ends installs (dpkg/lock is locked)"
    sleep 1
 done
 
-git clone git@gitlab.virtual.uniandes.edu.co:datacube-ideam/CDCol.git --branch desacoplado
-mv CDCol/* ~/
+# git clone git@gitlab.virtual.uniandes.edu.co:datacube-ideam/CDCol.git --branch desacoplado
+# mv CDCol/* ~/
 
-sudo apt install -y openssh-server postgresql-9.5 postgresql-client-9.5 postgresql-contrib-9.5 libgdal1-dev libhdf5-serial-dev libnetcdf-dev hdf5-tools netcdf-bin gdal-bin pgadmin3 libhdf5-doc netcdf-doc libgdal-doc git wget htop imagemagick ffmpeg|| exit 1
+sudo apt install -y \
+	openssh-server \
+	postgresql-9.5 \
+	postgresql-client-9.5 \
+	postgresql-contrib-9.5 \
+	libgdal1-dev \
+	libhdf5-serial-dev \
+	libnetcdf-dev \
+	hdf5-tools \
+	netcdf-bin \
+	gdal-bin \
+	pgadmin3 \
+	libhdf5-doc \
+	netcdf-doc \
+	libgdal-doc \
+	git \
+	wget  \
+	htop \
+	imagemagick ffmpeg|| exit 1
 
 
 #CONDA INSTALL
@@ -38,7 +56,7 @@ if ! hash "conda" > /dev/null; then
 fi
 
 source $HOME/.bashrc
-conda install -y python=3.6.8
+# conda install -y python=3.6.8
 conda install -y jupyter matplotlib scipy
 conda install -y psycopg2 gdal libgdal hdf5 rasterio netcdf4 libnetcdf pandas shapely ipywidgets scipy numpy
 
@@ -49,7 +67,7 @@ cat <<EOF >~/.datacube.conf
 db_database: datacube
 
 # A blank host will use a local socket. Specify a hostname to use TCP.
-db_hostname: $ipdb
+db_hostname: db
 
 # Credentials are optional: you might have other Postgres authentication configured.
 # The default username otherwise is the current user id.
@@ -87,9 +105,9 @@ sudo apt install nfs-common
 
 sudo chmod o+w /etc/fstab
 cat <<EOF >>/etc/fstab
-$ipnfs:/source_storage	/source_storage nfs 	defaults    	0   	0
-$ipnfs:/dc_storage		/dc_storage 	nfs 	defaults    	0   	0
-$ipnfs:/web_storage   	/web_storage	nfs 	defaults    	0   	0
+nfs:/source_storage	/source_storage nfs 	defaults    	0   	0
+nfs:/dc_storage		/dc_storage 	nfs 	defaults    	0   	0
+nfs:/web_storage   	/web_storage	nfs 	defaults    	0   	0
 EOF
 sudo chmod o-w /etc/fstab
 
@@ -100,12 +118,14 @@ sudo mount /source_storage
 sudo mount /web_storage
 
 #Configuracion del CRON de ingesta
+/home/cubo/anaconda/bin/pip install conda
 conda install -c conda-forge psycopg2 PyYAML
+
 git clone  git@gitlab.virtual.uniandes.edu.co:datacube-ideam/ingestion-scheduler.git --branch open_data_cube
 cd ingestion-scheduler
 cat <<EOF >settings.conf
 [database]
-host = $ipdb
+host = db
 port = 5432 
 name = ideam
 user = portal_web
