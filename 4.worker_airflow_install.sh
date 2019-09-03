025be7018ee937b94e4b81113e8919f3dec0db40
Aurelio Vivas <aa.vivas@uniandes.edu.co>
2019-09-03 08:13:25 -0500

    fixing

--- 4.worker_airflow_install.sh
+++ 4.worker_airflow_install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-if [[ $(id -u) -eq 0 ]] ; then echo "This script must  not be excecuted as root or using sudo(althougth the user must be sudoer and password will be asked in some steps)" ; exit 1 ; fi
+
+if [[ $(id -u) -eq 0 ]] ; then 
+	echo "This script must  not be excecuted \
+	as root or using sudo(althougth the user must \
+	be sudoer and password will be asked in some steps)" ; 
+	exit 1 ; 
+fi
 
 echo "¿Cuál es la ip del servidor de Bases de Datos?"
 read ipdb
@@ -13,6 +19,13 @@ read ipapi
 echo "¿Cuál es la ip del servidor AIRFLOW?"
 read ipairflow
 
+
+sudo sed -i "\$a$ipnfs   nfs" /etc/hosts
+sudo sed -i "\$a$ipdb    db" /etc/hosts
+sudo sed -i "\$a$ipapi   api" /etc/hosts
+sudo sed -i "\$a$ipairflow   airflow_server" /etc/hosts
+
+
 sudo apt-get update
 
 #VARIABLES
@@ -21,7 +34,7 @@ USUARIO_CUBO="$(whoami)"
 PASSWORD_CUBO='ASDFADFASSDFA'
 ANACONDA_URL="https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh"
 OPEN_DATA_CUBE_REPOSITORY="https://github.com/opendatacube/datacube-core.git"
-BRANCH="develop"
+BRANCH="datacube-1.6.1"
 
 
 while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
@@ -29,7 +42,25 @@ while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
    sleep 1
 done
 
-sudo apt install -y openssh-server postgresql-9.5 postgresql-client-9.5 postgresql-contrib-9.5 libgdal1-dev libhdf5-serial-dev libnetcdf-dev hdf5-tools netcdf-bin gdal-bin pgadmin3 libhdf5-doc netcdf-doc libgdal-doc git wget htop imagemagick ffmpeg|| exit 1
+sudo apt install -y \
+	openssh-server \
+	postgresql-9.5 \
+	postgresql-client-9.5 \
+	postgresql-contrib-9.5 \
+	libgdal1-dev \
+	libhdf5-serial-dev \
+	libnetcdf-dev \
+	hdf5-tools \
+	netcdf-bin \
+	gdal-bin \
+	pgadmin3 \
+	libhdf5-doc \
+	netcdf-doc \
+	libgdal-doc \
+	git \
+	wget \
+	htop \
+	imagemagick ffmpeg|| exit 1
 
 
 #CONDA INSTALL
@@ -48,14 +79,12 @@ conda install -y jupyter matplotlib scipy
 conda install -y psycopg2 gdal libgdal hdf5 rasterio netcdf4 libnetcdf pandas shapely ipywidgets scipy numpy
  
 
-
-
 cat <<EOF >~/.datacube.conf
 [datacube]
 db_database: datacube
 
 # A blank host will use a local socket. Specify a hostname to use TCP.
-db_hostname: $ipdb
+db_hostname: db
 
 # Credentials are optional: you might have other Postgres authentication configured.
 # The default username otherwise is the current user id.
@@ -73,16 +102,12 @@ datacube system check
 source $HOME/.bashrc
 cd $HOME
 
+# Airflow Install script
+/home/cubo/anaconda/bin/pip install conda
 conda install -y -c conda-forge psycopg2 redis-py flower celery=4.2
-conda install -y -c conda-forge "airflow==1.10.1"
-
-# To avoid this error
-# OSError: [Errno 13] Permiso denegado: 
-# '/home/cubo/.cache/pip/wheels/ab/4f/e6/....
-sudo chown -R cubo:cubo /home/cubo/.cache
-sudo chown -R cubo:cubo /home/cubo/.conda
-
-pip install "apache-airflow==1.10.2"
+/home/cubo/anaconda/bin/pip install "apache-airflow==1.10.2"
+# conda install -y -c conda-forge psycopg2 redis-py flower celery=4.2
+# conda install -y -c conda-forge "airflow==1.10.1"
 
 if [[ -z "${AIRFLOW_HOME}" ]]; then
     export AIRFLOW_HOME="$HOME/airflow"
@@ -90,18 +115,18 @@ if [[ -z "${AIRFLOW_HOME}" ]]; then
 fi
 
 airflow initdb
-sed -i "s%sql_alchemy_conn.*%sql_alchemy_conn = postgresql+psycopg2://airflow:$PASSWORD_AIRFLOW@$ipdb:5432/airflow%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%sql_alchemy_conn.*%sql_alchemy_conn = postgresql+psycopg2://airflow:$PASSWORD_AIRFLOW@db:5432/airflow%" "$AIRFLOW_HOME/airflow.cfg"
 sed -i "s%executor =.*%executor = CeleryExecutor%" "$AIRFLOW_HOME/airflow.cfg"
 
-sed -i "s%broker_url =.*%broker_url = amqp://airflow:airflow@$ipapi/airflow%" "$AIRFLOW_HOME/airflow.cfg"
-sed -i "s%result_backend =.*%result_backend = db+postgresql://airflow:$PASSWORD_AIRFLOW@$ipdb:5432/airflow%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%broker_url =.*%broker_url = amqp://airflow:airflow@api/airflow%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%result_backend =.*%result_backend = db+postgresql://airflow:$PASSWORD_AIRFLOW@db:5432/airflow%" "$AIRFLOW_HOME/airflow.cfg"
 
-sed -i "s%endpoint_url = .*%endpoint_url = http://$ipairflow:8080%" "$AIRFLOW_HOME/airflow.cfg"
-sed -i "s%base_url = .*%base_url = http://$ipairflow:8080%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%endpoint_url = .*%endpoint_url = http://airflow_server:8080%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%base_url = .*%base_url = http://airflow_server:8080%" "$AIRFLOW_HOME/airflow.cfg"
 
 sed -i "s%load_examples = .*%load_examples = False%" "$AIRFLOW_HOME/airflow.cfg"
 sed -i "s%base_log_folder = .*%base_log_folder = /web_storage/logs%" "$AIRFLOW_HOME/airflow.cfg"
-sed -i "s%dags_are_paused_at_creation = .*% = False%" "$AIRFLOW_HOME/airflow.cfg"
+sed -i "s%dags_are_paused_at_creation = .*%dags_are_paused_at_creation = False%" "$AIRFLOW_HOME/airflow.cfg"
 
 #MOUNT NFS SERVER
 cd $HOME
@@ -109,9 +134,9 @@ cd $HOME
 sudo apt install -y nfs-common
 sudo chmod o+w /etc/fstab
 cat <<EOF >>/etc/fstab
-$ipnfs:/source_storage	/source_storage nfs 	defaults    	0   	0
-$ipnfs:/dc_storage		/dc_storage 	nfs 	defaults    	0   	0
-$ipnfs:/web_storage   	/web_storage	nfs 	defaults    	0   	0
+nfs:/source_storage	/source_storage nfs 	defaults    	0   	0
+nfs:/dc_storage		/dc_storage 	nfs 	defaults    	0   	0
+nfs:/web_storage   	/web_storage	nfs 	defaults    	0   	0
 EOF
 sudo chmod o-w /etc/fstab
 
